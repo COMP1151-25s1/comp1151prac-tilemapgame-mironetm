a4fefdb26e60e1d366f92ce9df12d76b082dbc61
mironetm <mironet2001@gmail.com>
2025-05-06 00:02:30 +1000

    Player flip
    Making the player flip to left.
    Creating the canvas for health

---
+++ Assets/TileMap/tiles.tsx
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tilemap_packed" tilewidth="16" tileheight="16" tilecount="132" columns="12">
+ <image source="tilemap_packed.png" width="192" height="176"/>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0320205" y="9.15786" width="16" height="6.81012"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0.0320205" width="16" height="8.98751"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10.0224" y="0" width="5.97759" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="10.0544" width="16" height="5.94557"/>
+  </objectgroup>
+ </tile>
+ <tile id="26">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="8.98751"/>
+   <object id="2" x="0" y="0" width="16" height="9.01953"/>
+  </objectgroup>
+ </tile>
+ <tile id="40">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="16" height="16.032"/>
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="unity:IsTrigger" value="true"/>
+   <property name="unity:layer" value="Hazard"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+ </tile>
+</tileset>
