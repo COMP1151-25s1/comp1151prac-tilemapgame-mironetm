87c2c32d7a8567d0b5abed63ef054677a65e7828
mironetm <mironet2001@gmail.com>
2025-05-26 09:17:35 +1000

    linearvelocity

--- Assets/TileMap/Racing Pack/spritesheet_tiles.tsx
+++ Assets/TileMap/Racing Pack/spritesheet_tiles.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="spritesheet_tiles" tilewidth="128" tileheight="128" tilecount="324" columns="18">
  <image source="../../../../kenney_racing-pack/Spritesheets/spritesheet_tiles.png" width="2304" height="2304"/>
+ <tile id="48">
+  <properties>
+   <property name="unity:IsTrigger" value="true"/>
+   <property name="unity:layer" value="grass"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="128" height="128"/>
+   <object id="2" x="0" y="0" width="128" height="128"/>
+  </objectgroup>
+ </tile>
 </tileset>
